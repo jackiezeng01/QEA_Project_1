777abc0096225069fe3dbed7672395711aa8cdf8
Lieselotte Heinrich <lheinrich@olin.edu>
2020-02-25 22:29:11 +0900

    added more funcs for data processing

--- airbnb_project_qea.m
+++
@@ -1,9 +0,0 @@
-%%
-data = airbnblistings2;
-data.Properties.VariableNames(73);
-data_sorted = sortrows(data, 73, 'descend');
-data_sorted = data_sorted(3:size(data_sorted, 1), :);
-
-data_clean = data_sorted(1:15734, :);
-data_clean = removevars(data_clean,{'Neighbourhood', 'NeighbourhoodGroupCleansed', 'SquareFeet', 'WeeklyPrice', 'MonthlyPrice', 'CalendarUpdated', 'HostNeighbourhood', 'HostURL', 'ListingUrl', 'ScrapeID', 'LastScraped', 'Summary', 'Space', 'Description', 'ExperiencesOffered', 'NeighborhoodOverview', 'Notes', 'Transit', 'Access', 'HouseRules', 'Interaction', 'ThumbnailUrl', 'MediumUrl', 'PictureUrl', 'XLPictureUrl', 'HostThumbnailUrl', 'HostAbout', 'Geolocation', 'JurisdictionNames', 'License', 'HostPictureUrl', 'Availability365', 'Availability90', 'Availability60', 'Availability30', 'HasAvailability'});
-data_clean(1:10,:)
